6d715bd76fb66cea86eddbcea328dfe7ef776ee8
anjanpaul <anjanpaul2lrp@gmail.com>
2021-03-24 04:12:09 +0600

    Template Django

---
+++ myapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'myapp/home.html')
+
+def another(request):
+    return render(request, 'myapp/about.html')
